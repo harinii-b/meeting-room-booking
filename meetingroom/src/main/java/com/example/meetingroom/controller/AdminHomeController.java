0bcce6cad16a35befb6e9e3690c0330cff931c91
harinii-b <harinib1803@gmail.com>
2025-04-07 12:41:36 +0530

    added admin home

---
+++ meetingroom/src/main/java/com/example/meetingroom/controller/AdminHomeController.java
@@ -0,0 +1,25 @@
+// package com.example.meetingroom.controller;
+
+// public class AdminHomeController {
+    
+// }
+
+// package com.example.meetingroom.controller;
+
+// public class UserHomeController {
+    
+// }
+
+package com.example.meetingroom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminHomeController {
+    
+    @GetMapping("/adminhome")
+    public String showAdminHomePage() {
+        return "adminhome"; // This will render .html from templates folder
+    }
+}
